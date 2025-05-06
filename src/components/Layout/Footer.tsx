39c323eb5aa7a3f181633d4927abfd31c35025bc
komiwalnut <62009152+komiwalnut@users.noreply.github.com>
2025-05-06 08:38:30 +0800

    Update Footer.tsx

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -2,17 +2,6 @@ export function Footer() {
   return (
     <footer>
       <div className="container mx-auto">
-        <p>
-          Wild Forest Lords Dashboard | Staking Contract:{' '}
-          <a 
-            href={`https://explorer.roninchain.com/address/0xfb597d6fa6c08f5434e6ecf69114497343ae13dd`}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="contract-link"
-          >
-            0xfb597d6fa6c08f5434e6ecf69114497343ae13dd
-          </a>
-        </p>
         <p className="mt-2">
           © {new Date().getFullYear()} | Made with ❤️ by Player 222 (<a href="https://explorer.roninchain.com/address/medeasolon.ron" className="contract-link">medeasolon.ron</a>)
         </p>
