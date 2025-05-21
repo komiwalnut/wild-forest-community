1d452d42f705124da0b9bbfa96dba6fe35ed7666
komiwalnut <62009152+komiwalnut@users.noreply.github.com>
2025-05-21 11:28:24 +0800

    Update Footer.tsx

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
     <footer>
       <div className="container mx-auto">
         <p className="mt-2">
-          © {new Date().getFullYear()} | Made with ❤️ by Player 222 (<a href="https://explorer.roninchain.com/address/medeasolon.ron" className="contract-link">medeasolon.ron</a>)
+          © {new Date().getFullYear()} | Made with ❤️ by Player 222 (<a href="https://explorer.roninchain.com/address/walnut.ron" className="contract-link">walnut.ron</a>)
         </p>
       </div>
     </footer>
